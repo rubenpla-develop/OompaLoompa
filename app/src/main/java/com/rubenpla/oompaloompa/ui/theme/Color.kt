d9ae2e52f61c97ce9170b068c85bc59cef896d9c
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-07-15 10:04:57 +0200

    resources: add several color resources

--- app/src/main/java/com/rubenpla/oompaloompa/ui/theme/Color.kt
+++ app/src/main/java/com/rubenpla/oompaloompa/ui/theme/Color.kt
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//extra colors
+val PinkA400 = Color(0xFFF50057)
+val PinkA700 = Color(0xFFC51162)
+val Black = Color(0xFF000000)
+val Grey850 = Color(0xFF303030)
+val White = Color(0xFFFFFFFF)
