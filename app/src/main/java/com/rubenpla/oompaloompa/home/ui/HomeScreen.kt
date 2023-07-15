2893430758a66aaefea6df7488d843a57c04af68
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-07-15 11:48:41 +0200

    feat: add loader view for Worker list loading

--- app/src/main/java/com/rubenpla/oompaloompa/home/ui/HomeScreen.kt
+++ app/src/main/java/com/rubenpla/oompaloompa/home/ui/HomeScreen.kt
@@ -15,30 +15,36 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.paging.LoadState
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.rememberImagePainter
 import com.rubenpla.oompaloompa.R
 import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.home.domain.entity.EmployeeResultsEntity
 import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.home.ui.viewModel.EmployeeListViewModel
 import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.home.ui.viewModel.HomeState
+import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.ui.common.LoadingItem
 import com.rubenpla.oompaloompa.ui.common.HomeAppBar
 import com.rubenpla.oompaloompa.ui.theme.PurpleGrey40
 
@@ -82,13 +88,28 @@ fun WorkersGridList(viewModel: EmployeeListViewModel, paddingValues: PaddingValu
                         EmployeeItem(employee = employeeEntity)
                     }
                 }
+
+                employeeItems.apply {
+                    when {
+                        loadState.refresh is LoadState.Loading -> {
+                            item { LoadingItem() }
+                            item { LoadingItem() }
+                        }
+
+                        loadState.append is LoadState.Loading -> {
+                            /*item { LoadingItem() }
+                            item { LoadingItem() }*/
+                        }
+
+                        loadState.refresh is LoadState.Error -> {}
+                        loadState.append is LoadState.Error -> {}
+                    }
+                }
             }
         }
 
         else -> {}
     }
-
-
 }
 
 @Composable
