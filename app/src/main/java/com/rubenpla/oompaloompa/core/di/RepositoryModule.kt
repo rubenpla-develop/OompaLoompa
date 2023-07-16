a32161a9d0c0f68533cc6a4a49f9fa3774599a31
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-07-16 17:43:09 +0200

    chore : Update DI Modules
    - Add new EMployeeDetailsModule

--- app/src/main/java/com/rubenpla/oompaloompa/core/di/RepositoryModule.kt
+++ app/src/main/java/com/rubenpla/oompaloompa/core/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.core.di
 
-import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.home.data.DataSource
+import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.data.DataSource
 import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.home.data.repository.EmployeeRepositoryImpl
 import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.home.domain.repository.EmployeeRepository
+import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.profile.data.repository.EmployeeDetailRepositoryImpl
+import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.profile.domain.repository.EmployeeDetailRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -16,4 +18,7 @@ object RepositoryModule {
     fun providesEmployeeRepository(dataSource: DataSource): EmployeeRepository =
         EmployeeRepositoryImpl(dataSource = dataSource)
 
+    @Provides
+    fun provideEmployeeDetailRepository(dataSource : DataSource) : EmployeeDetailRepository =
+        EmployeeDetailRepositoryImpl(dataSource = dataSource)
 }
