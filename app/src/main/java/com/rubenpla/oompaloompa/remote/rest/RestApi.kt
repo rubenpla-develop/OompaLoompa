fb0d970cad11c7f59f42876cc27cb2b9bbbc16e3
Ruben Pla Ferrero <rubenpla.develop@gmail.com>
2023-07-16 17:46:37 +0200

    feat: update Remote layer with EmployeeDetail feature

--- app/src/main/java/com/rubenpla/oompaloompa/remote/rest/RestApi.kt
+++ app/src/main/java/com/rubenpla/oompaloompa/remote/rest/RestApi.kt
@@ -1,9 +1,13 @@
 package com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.remote.rest
 
-import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.home.data.request.GetAllEmployeesRequest
-import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.home.data.response.EmployeeListResponse
+import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.home.remote.request.GetAllEmployeesRequest
+import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.home.remote.response.EmployeeListResponse
+import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.profile.remote.request.GetEmployeeDetailRequest
+import com.rubenpla.oompaloompa.com.rubenpla.oompaloompa.profile.remote.response.EmployeeDetailResponse
 
 interface RestApi {
     suspend fun getEmployees(getAllEmployeesRequest: GetAllEmployeesRequest): EmployeeListResponse
+
+    suspend fun getEmployeeDetail(getEmployeeDetailRequest: GetEmployeeDetailRequest): EmployeeDetailResponse
 
 }
